4c6255f9a44847e6f5a4348130878e19c9fe80eb
paul anyel diaz patiño <paulangel826@gmail.com>
2023-08-03 11:19:53 -0500

    Actulizacion de ESPE LAB

--- email/assets/js/checkbox.js
+++
@@ -1,7 +0,0 @@
-$('.checkbox').click(function(){
-    if ($('input.checkbox').is(':checked')) {
-        $(".theme").attr("href", "assets/css/dark-theme.css");
-    }else{
-        $(".theme").attr("href", "assets/css/light-theme.css");
-    }
-});
